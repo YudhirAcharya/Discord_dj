494981cb43b6b76d0c7e5b7b1095217a12d7fa28
Yudhir <077bct047.yudhir@sagarmatha.edu.np>
2024-06-25 21:39:14 +0545

    1 hour

--- discord/base/views.py
+++ discord/base/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+rooms = [
+    {'id':1, 'name':'Python', 'description':'This is room 1'},
+    {'id':2, 'name':'JS', 'description':'This is room 2'},
+    {'id':3, 'name':'SQL', 'description':'This is room 3'},
 
-def baseHome(request):
-    return HttpResponse("Hello, world. You're at the discord base page.")
+]
+
+def home(request):
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context)
+
+# def home(request):
+#     return render(request, 'home.html', {'rooms':rooms})
+
+def room(request, pk):
+    return render(request, 'base/room.html')

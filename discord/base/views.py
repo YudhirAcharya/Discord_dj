028641ff8e394b78b3f5ad4ccd84af36a5604cc8
Yudhir <077bct047.yudhir@sagarmatha.edu.np>
2024-06-30 06:21:58 +0545

    rooms

--- discord/base/views.py
+++ discord/base/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
-
+from .models import Room
 # Create your views here.
 
-rooms = [
-    {'id':1, 'name':'Python', 'description':'This is room 1'},
-    {'id':2, 'name':'JS', 'description':'This is room 2'},
-    {'id':3, 'name':'SQL', 'description':'This is room 3'},
+# rooms = [
+#     {'id':1, 'name':'Python', 'description':'This is room 1'},
+#     {'id':2, 'name':'JS', 'description':'This is room 2'},
+#     {'id':3, 'name':'SQL', 'description':'This is room 3'},
 
-]
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)
 
@@ -17,4 +18,8 @@ def home(request):
 #     return render(request, 'home.html', {'rooms':rooms})
 
 def room(request, pk):
-    return render(request, 'base/room.html')
+
+    room = Room.objects.get(id = pk)
+            
+    context = {'room':room}
+    return render(request, 'base/room.html', context)
--- discord/discord/urls.py
+++ discord/discord/urls.py
@@ -21,6 +21,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-   path('', include('base.urls'))
+    path('', include('base.urls'))
     
 ]
